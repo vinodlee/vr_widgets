0a42c7f60b3cca00bed37b64a4d5f654870ff14f
Vinoth M <vinothcse14325@gmail.com>
2022-03-22 16:18:17 +0530

    first commit

---
+++ lib/src/viru.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ViruContainer extends StatefulWidget {
+  const ViruContainer({Key? key}) : super(key: key);
+
+  @override
+  State<ViruContainer> createState() => _ViruContainerState();
+}
+
+class _ViruContainerState extends State<ViruContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
